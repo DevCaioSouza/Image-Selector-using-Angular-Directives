1c074de91ff5e2cd9871bbf4ac69a3ccf8a77372
caio souza <caiocbahia@hotmail.com>
2022-05-08 19:32:05 -0300

    getting some data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_image_selector';
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE'
+    },
+    {
+      title: 'The forest',
+      url: 'https://i.picsum.photos/id/1023/3955/2094.jpg?hmac=AW_7mARdoPWuI7sr6SG8t-2fScyyewuNscwMWtQRawU'
+    },
+    {
+      title: 'Dried land',
+      url: 'https://i.picsum.photos/id/1028/5184/3456.jpg?hmac=WhttNfn25eTgLTNnhRujSq4IVjx2mMa6wvPG1c6qMVc'
+    },
+    {
+      title: 'Town Park',
+      url: 'https://i.picsum.photos/id/1029/4887/2759.jpg?hmac=uMSExsgG8_PWwP9he9Y0LQ4bFDLlij7voa9lU9KMXDE'
+    },
+  ]
 }
